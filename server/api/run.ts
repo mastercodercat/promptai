d426aad9a1265dd938ec3bfef55f593a7d74ebb2
mastercodercat <shinyhenryhan@gmail.com>
2023-06-16 12:46:00 +0200

    open ai api key check by console log for deployment error

--- server/api/run.ts
+++ server/api/run.ts
@@ -1,6 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 export default defineEventHandler(async (event) => {
+  console.log(process.env.OPENAI_API_KEY);
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
